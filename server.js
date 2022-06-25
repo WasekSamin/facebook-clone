767fe2594801fcc24d0f821e16586c1b2ee906ef
WasekSamin <wasek.samin47@gmail.com>
2022-06-25 13:34:19 -0700

    25th june

--- server.js
+++ server.js
@@ -5,9 +5,19 @@ const io = require("socket.io")(9000, {
 });
 
 io.on("connection", (socket) => {
-    const userToken = socket.handshake.query.userToken;
+  const userToken = socket.handshake.query.userToken;
 
-    socket.join(userToken);
+  socket.join(userToken);
 
+  socket.on("post-created", (postObj) => {
+    if (postObj) {
+      socket.broadcast.emit("receive-post", postObj);
+    }
+  });
 
-})
+  socket.on("update-account-data", (accountObj) => {
+    if (accountObj) {
+      socket.broadcast.emit("receive-account-update-data", accountObj);
+    }
+  });
+});
