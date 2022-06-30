b8b4f260350938ab6f44a1d31d4997fef1a9d6e1
WasekSamin <wasek.samin47@gmail.com>
2022-06-30 01:59:37 -0700

    29th june

--- server.js
+++ server.js
@@ -20,4 +20,20 @@ io.on("connection", (socket) => {
       socket.broadcast.emit("receive-account-update-data", accountObj);
     }
   });
+
+  socket.on("send-friend-request", (friendRequestData) => {
+    if (friendRequestData) {
+      const room = friendRequestData.receiverToken;
+      socket.join(room);
+      socket.to(room).emit("receive-friend-request", friendRequestData);
+    }
+  })
+
+  socket.on("sender-delete-friend-request", friendRequestData => {
+    if (friendRequestData) {
+      const room = friendRequestData.receiverToken;
+      socket.join(room);
+      socket.to(room).emit("friend-request-deleted-from-receiver", friendRequestData);
+    }
+  })
 });
