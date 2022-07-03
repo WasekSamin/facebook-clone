d3d552b6d61bd6b8da6ea3b1db9cfdb2efa2b127
WasekSamin <wasek.samin47@gmail.com>
2022-07-03 12:09:03 -0700

    3th july

--- server.js
+++ server.js
@@ -27,13 +27,57 @@ io.on("connection", (socket) => {
       socket.join(room);
       socket.to(room).emit("receive-friend-request", friendRequestData);
     }
-  })
+  });
 
-  socket.on("sender-delete-friend-request", friendRequestData => {
+  socket.on("sender-delete-friend-request", (friendRequestData) => {
     if (friendRequestData) {
       const room = friendRequestData.receiverToken;
       socket.join(room);
-      socket.to(room).emit("friend-request-deleted-from-receiver", friendRequestData);
+      socket
+        .to(room)
+        .emit("friend-request-deleted-from-receiver", friendRequestData);
     }
-  })
+  });
+
+  socket.on("receiver-delete-friend-request", (friendRequestData) => {
+    if (friendRequestData) {
+      const room = friendRequestData.receiverToken;
+      socket.join(room);
+      socket
+        .to(room)
+        .emit("friend-request-deleted-by-receiver", friendRequestData);
+
+      const receiverRoom = friendRequestData.userToken;
+      socket.join(receiverRoom);
+      socket
+        .to(receiverRoom)
+        .emit("friend-request-deleted-by-receiver", friendRequestData);
+    }
+  });
+
+  socket.on("receiver-accept-friend-request", (friendRequestData) => {
+    if (friendRequestData) {
+      const room = friendRequestData.receiverToken;
+      socket.join(room);
+      socket
+        .to(room)
+        .emit("friend-request-accepted-by-receiver", friendRequestData);
+
+      const receiverRoom = friendRequestData.userToken;
+      socket.join(receiverRoom);
+      socket.to(receiverRoom).emit("friend-request-accepted-by-receiver", friendRequestData);
+    }
+  });
+
+  socket.on("remove-user-from-friend-list", (friendData) => {
+    if (friendData) {
+      const room = friendData.receiverToken;
+      socket.join(room);
+      socket.to(room).emit("friend-removed-from-user-list", friendData);
+
+      const userRoom = friendData.userToken;
+      socket.join(userRoom);
+      socket.to(userRoom).emit("friend-removed-from-user-list", friendData);
+    }
+  });
 });
